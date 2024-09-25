0984b5c94b8ea67798e4cf02eca3410da600a7c9
Gustafff <gustafff93s@gmail.com>
2024-09-25 17:35:42 -0600

    quitando imagen del reporte

--- inventario_deli_tutti/models/reportes/reporte_inventario.py
+++ inventario_deli_tutti/models/reportes/reporte_inventario.py
@@ -1,55 +1,52 @@
 from odoo import models
 import logging
+
 _logger = logging.getLogger(__name__)
 
+
 class ReporteInventario(models.AbstractModel):
-    _name = 'report.inventario_deli_tutti.report_inventario'
-    _description = 'Reporte de Inventario Deli Tutti'
+    _name = "report.inventario_deli_tutti.report_inventario"
+    _description = "Reporte de Inventario Deli Tutti"
 
     def _get_report_values(self, docids, data=None):
         if not docids:
-            docids = self.env['inventario.product'].search([]).ids
-        productos = self.env['inventario.product'].browse(docids)
-        
+            docids = self.env["inventario.product"].search([]).ids
+        productos = self.env["inventario.product"].browse(docids)
+
         productos_con_lotes = []
         total_inventario = 0
         for producto in productos:
             total_costo = 0
             lotes = []
-            for linea in self.env['inventario.entrada.linea'].search([('producto_id', '=', producto.id)]):
-                lotes.append({
-                    'lote': linea.lote,
-                    'cantidad': linea.cantidad,
-                    'costo_unitario': linea.costo_unitario
-                })
+            for linea in self.env["inventario.entrada.linea"].search(
+                [("producto_id", "=", producto.id)]
+            ):
+                lotes.append(
+                    {
+                        "lote": linea.lote,
+                        "cantidad": linea.cantidad,
+                        "costo_unitario": linea.costo_unitario,
+                    }
+                )
                 total_costo += linea.cantidad * linea.costo_unitario
-            
-            productos_con_lotes.append({
-                'item': producto.item,
-                'descripcion': producto.descripcion,
-                'um': producto.um,
-                'existencia': producto.existencia,
-                'lotes': lotes,
-                'total_costo': total_costo,
-            })
+
+            productos_con_lotes.append(
+                {
+                    "item": producto.item,
+                    "descripcion": producto.descripcion,
+                    "um": producto.um,
+                    "existencia": producto.existencia,
+                    "lotes": lotes,
+                    "total_costo": total_costo,
+                }
+            )
 
             total_inventario += total_costo
 
-        # Definir el nombre de la compañía y la ruta del logo
-        company_name = "Deli Tutti"
-        logo_url = '/inventario_deli_tutti/static/src/img/logo.jpg'
-
-        # Log para verificar que los valores se están pasando correctamente
-        _logger.info(f"Logo URL: {logo_url}")
-        _logger.info(f"Company Name: {company_name}")
-
         return {
-            'docs': productos_con_lotes,
-            'total_inventario': total_inventario,
-            'company_name': company_name,
-            'logo_url': logo_url,
+            "docs": productos_con_lotes,
+            "total_inventario": total_inventario
         }
-
 
 
 # from odoo import models
@@ -69,4 +66,3 @@ class ReporteInventario(models.AbstractModel):
 #         return {
 #             'docs': docs,
 #         }
-
